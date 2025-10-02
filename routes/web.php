63b9836d1b41b304374fb2e7854abb966503b594
ali <ali-ibrahim94@hotmail.com>
2025-10-02 18:19:55 +0300

    add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,91 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get("route1",function(){
+  return "I am your first route";
+});
+
+Route::get("route2",function(){
+   return  123;
+});
+
+Route::get("route3",function(){
+   return response()->json(["course"=>"Web 2","nbOFCredits"=>3]);
+});
+
+Route::get("route4",function(Request $request){
+   if($request->header("token")=="123456"){
+       return "ok accepted";
+   }else{
+       return "404";
+   }
+});
+
+Route::get("route5",function(){
+   return response()->json(["data"=>"this is my data"])
+   ->header("message1","this is my message from server")
+   ->header("message2","this is my second message") ;
+});
+
+Route::get("route6",function(){
+    return response()->json(["data"=>"this is my data"])
+        ->withHeaders([
+            'h1'=>"message 1",
+            'h2'=>"messsage 2"
+        ]);
+});
+
+
+Route::get("route7",function(){
+    $data = [
+        "name"=>"ali ibrahim"
+    ];
+   return response($data,Response::HTTP_NO_CONTENT);
+});
+
+Route::get("route8",function(){
+    $data = [
+        "name"=>"ali ibrahim"
+    ];
+    return response($data,201);
+});
+
+Route::get("route-param/{id}",function ($id){
+    return "the id is : " . $id;
+});
+
+Route::get("route-param2/{id}/{name}",function($test1, $tester){
+   return "the name is:" . $tester . "   and the id is: " . $test1;
+});
+
+Route::get("route-param3/{id?}",function ($id=0){
+    if($id==0){
+        return abort(403);
+    }else{
+        return "the id is : " . $id;
+    }
+
+});
+
+
+Route::get("route-param5/{id1?}/{id2?}",function($id1, $id2=0){
+    return "the id is:" . $id1 . "   and the id2 is: " . $id2;
+});
+
+
+Route::get("route-param6",function(Request  $request){
+    $id1 = $request->input('id1',0);
+    $id2 =  $request->id2;
+    return "the id is:" . $id1 . "   and the id2 is: " . $id2;
+});
+
+Route::post("post-route",function(Request  $request){
+   return "i am post route";
 });
